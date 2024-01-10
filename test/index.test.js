12df99f1e453b5584cf351f40b3f2e604b3f0814
farizi567 <malfarizisidiqrizki015@gmail.com>
2024-01-10 19:26:31 +0700

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+// ! Dont change this code
+const {
+  fetchProductsData,
+  setProductsCards,
+  convertToRupiah,
+  countDiscount,
+} = require("../src/index.js");
+const cartData = require("../src/data/cart.js");
+
+// @ Write your code here
+
+// Asyncronous Testing
+// https://jestjs.io/docs/asynchronous
+
+
+// Mocking
+// https://jestjs.io/docs/mock-functions
+
+
+// Setup & Teardown
+// https://jestjs.io/docs/setup-teardown
+
+// index.test.js
+
+test('Traininig-jest', () => {
+  expect(1 + 1).toBe(2);
+});
